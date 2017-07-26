48f27e91e9457d37437cd4008cf7d87106662c13
Nan Jiang <nanjiang@pc76.lfb.rwth-aachen.de>
2017-07-26 09:28:59 +0200

    update the new changes to create_unet

--- create_unet.py
+++ create_unet.py
@@ -17,7 +17,7 @@ class Unet(object):
         # down path image lists
         img_dlist = []
         img_dlist.append(image)
-        for i = xrange(4):
+        for i in xrange(4):
             tmp = img_list[i]
             tmp = tf.layers.conv2d(inputs=tmp, filters=2 ** (i+6), kernel_size=3, padding="same", activation=tf.nn.relu)
             tmp = tf.layers.conv2d(inputs=tmp, filters=2 ** (i+6), kernel_size=3, padding="same",  activation=tf.nn.relu)
@@ -54,18 +54,70 @@ class Unet(object):
     '''
     these functions all use the attributes written under tf.nn where weights and bias are visible 
     '''
-    def downnetwork(self, weights, bias, image):
+    def downnetwork(self, weights, image):
         ### TODO ###
+        img_list = []
+        img_list.append(image)
+        for i in xrange(5):
+            tmp = img_list[i]
+            tmp = tf.nn.conv2d(tmp, weights, strides=[1,1,1,1], padding="SAME")
+            tmp = tf.nn.relu(tmp)
+            tmp = tf.nn.conv2d(tmp, weights, strides=[1,1,1,1], padding="SAME")
+            tmp = tf.nn.relu(tmp)
+            tmp = tf.nn.max_pool(tmp, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME")
+            img_dist.append(tmp)
+        tmp = img_dist[3]
+        tmp = tf.nn.conv2d(tmp, weights, strides=[1,1,1,1], padding="SAME")
+        tmp = tf.nn.relu(tmp)
+        tmp = tf.nn.conv2d(tmp, weights, strides=[1,1,1,1], padding="SAME")
+        tmp = tf.nn.relu(tmp)
+        img_dist.append(tmp)
         return img_dlist
 
-    def upnetwork(self, weights, bias, img_dlist):
+    def upnetwork(self, weights, img_dlist):
         ### TODO ###
+        tmp = img_dlist[4]
+        for i in xrange(4,0,-1):
+            tmp_shape = tf.shape(tmp)
+            tmp = tf.nn.conv2d_transpose(tmp, weights, tf.stack([tmp_shape[0], 2*tmp_shape[1], 2*tmp_shape[2], tmp_shape[3]/2]), strides = [1, 2, 2, 1])
+            tmp = tf.concat([img_dist[i-1], tmp]. axis=3)
+            tmp = tf.nn.conv2d(tmp, weights, strides=[1,1,1,1], padding="SAME")                                                                                                                                    
+            tmp = tf.nn.relu(tmp)                                                                                                                                                                                  
+            tmp = tf.nn.conv2d(tmp, weights, strides=[1,1,1,1], padding="SAME")                                                                                                                                    
+            tmp = tf.nn.relu(tmp) 
+        img_u = tmp
         return img_u
 
-    def outputnetwork_segmentation(self, weights, bias, img_u):
+    def outputnetwork_segmentation(self, weights, img_u):
         ### TODO ###
+        img_seg = tf.nn.conv2d(img_u, weights, strides=[1,1,1,1], padding="SAME")
         return img_seg
 
-    def network_segmentation(self, weights, bias):
+    def network_segmentation(self, weights):
         ### TODO ###
+        img_dist = self.downnetwork(weights, self.image)
+        image_u = self.upnetwork(weights, img_dlist)
+        img_seg = self.outputnetwork_segmentation(weights, img_u)
         return img_seg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
