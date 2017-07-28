d776e707f0e9c7ea0232ef866584570a2db4de42
Nan Jiang <nanjiang@pc76.lfb.rwth-aachen.de>
2017-07-28 19:02:43 +0200

    self update

--- create_unet.py
+++ create_unet.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 import numpy as np
 
 class Unet(object):
-    def  __init__(self, image, n_class):
-        self.batch_size = image[0]
-        self.img_size_x = image[1]
-        self.img_size_y = image[2]
+    def  __init__(self, img_shape, n_class):
+        self.batch_size = img_shape[0]
+        self.img_size_x = img_shape[1]
+        self.img_size_y = img_shape[2]
         self.image = tf.placeholder("float", shape=[self.batch_size, self.img_size_x, self.img_size_y, 1])
         self.n_class = n_class
-
+        self.img_seg = self.network_segmentation()
     '''
     these functions all use the methods written in tf.layers where weights and bias are not visible
     '''
@@ -16,14 +16,14 @@ class Unet(object):
     # initialize the weights
     # create a variable w = tf.Variable(<initail value>, name=<optional-name>)
     def weights_create(self, shape, stddev=0.2, name=None):
-        initial = tf.truncated_normal(shape, stddev=stddev)
+        initial = tf.truncated_normal(shape=shape, stddev=stddev)
         if name is None:
             return tf.Variable(initial)
         else:
             return tf.get_variable(name, initializer=initial)
 
     def bias_create(self, shape, name=None):
-        initial = tf.constant(0.0, shape)
+        initial = tf.constant(0.0, shape=shape)
         if name is None:
             return tf.Variable(initial)
         else:
@@ -72,8 +72,8 @@ class Unet(object):
     these functions all use the attributes written under tf.nn where weights and bias are visible 
     '''
     def downnetwork(self, image):
-        img_list = []
-        img_list.append(image)
+        img_dlist = []
+        img_dlist.append(image)
         with tf.variable_scope("downwards"):
             for i in xrange(5):
                 if i == 0:
@@ -84,27 +84,26 @@ class Unet(object):
                 bias1 = self.bias_create(shape=[2**(i+6)], name="db"+str(i)+"1")
                 bias2 = self.bias_create(shape=[2**(i+6)], name="db"+str(i)+"2")
 
-                tmp = img_list[i]
+                tmp = img_dlist[i]
                 tmp = tf.nn.conv2d(tmp, weight1, strides=[1,1,1,1], padding="SAME")
                 tmp = tf.nn.relu(tmp+bias1)
                 tmp = tf.nn.conv2d(tmp, weight2, strides=[1,1,1,1], padding="SAME")
                 tmp = tf.nn.relu(tmp+bias2)
                 if i != 4:
                     tmp = tf.nn.max_pool(tmp, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME")
-                img_dist.append(tmp)
+                img_dlist.append(tmp)
             
         return img_dlist
 
     def upnetwork(self, img_dlist):
-        tmp = img_dlist[4]
+        tmp = img_dlist[5]
         with tf.variable_scope("upwards"):
             for i in xrange(4,0,-1):
-                tmp_shape = tf.shape(tmp)
-                upfilter = self.weights_create(shape=[3,3,tmp_shape[3]/2,tmp_shape[3]], name="uf"+str(i))
-                upb = self.bias_create(shape=[tmp_shape[3]/2], name="upb"+str(i))
-                tmp = tf.nn.conv2d_transpose(tmp, upfilter, tf.stack([tmp_shape[0], 2*tmp_shape[1], 2*tmp_shape[2], tmp_shape[3]/2]), strides = [1, 2, 2, 1])
+                upfilter = self.weights_create(shape=[3,3,2**(i+5),2**(i+6)], name="uf"+str(i))
+                upb = self.bias_create(shape=[2**(i+5)], name="upb"+str(i))
+                tmp = tf.nn.conv2d_transpose(tmp, upfilter, tf.stack([self.batch_size, self.img_size_x/(2**(i-1)), self.img_size_y/(2**(i-1)), 2**(i+5)]), strides = [1, 2, 2, 1])
                 tmp = tf.nn.relu(tmp+upb)
-                tmp = tf.concat([img_dist[i-1], tmp]. axis=3)
+                tmp = tf.concat([img_dlist[i], tmp], axis=3)
                 weight1 = self.weights_create(shape=[3,3,2**(i+6),2**(i+5)], name="uw1"+str(i))
                 bias1 = self.bias_create(shape=[2**(i+5)], name="ub1"+str(i))
                 tmp = tf.nn.conv2d(tmp, weight1, strides=[1,1,1,1], padding="SAME") 
@@ -124,7 +123,7 @@ class Unet(object):
         return img_seg
 
     def network_segmentation(self):
-        img_dist = self.downnetwork(self.image)
+        img_dlist = self.downnetwork(self.image)
         image_u = self.upnetwork(img_dlist)
         img_seg = self.outputnetwork_segmentation(img_u)
         return img_seg
@@ -132,14 +131,16 @@ class Unet(object):
 
 
 class Trainer(object):
-    def __init__(self, img_seg, n_class, learning_rate, global_step):
+    def __init__(self, img_seg, n_class, learning_rate):
         self.img_seg = tf.reshape(img_seg, [-1, n_class])
         self.label = tf.placeholder("float", shape=[None,None,None,n_class])
         self.loss = self.loss()
-        self.optimizer = self.train(learning_rate, global_step)
+        self.global_step = tf.Variable(0)
+        self.optimizer = self.train(learning_rate, self.global_step)
 
     def loss(self):
         loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.label, logits=self.img_seg)
+        loss = tf.reduce_mean(loss)
         return loss
 
     def train(self, learning_rate, global_step):
--- unit_test.py
+++ unit_test.py
@@ -1,14 +1,39 @@
 # it is for the test of the functions written
 from read_images import ImageReader, LabelReader
+from create_unet import Unet, Trainer
+import tensorflow as tf
 
-# testing read_images
-batchsize = 4
-path = "/home/students/nanjiang/Data/unit_test/label/*.tif"
-labelreader = LabelReader(path, batchsize)
-label = labelreader.read_batchlabels()
-print(label.shape)
-print(label[0].shape)
-print(label[1].shape)
+FLAGS = tf.flags.FLAGS
+tf.flags.DEFINE_string('mode', "training", "Mode read_image read_labels training")
+
+if FLAGS.mode == "read_image":
+    # testing read_images
+    batchsize = 4
+    path = "/home/students/nanjiang/Data/unit_test/label/*.tif"
+    labelreader = LabelReader(path, batchsize)
+    label = labelreader.read_batchlabels()
+    print(label.shape)
+    print(label[0].shape)
+    print(label[1].shape)
+
+if FLAGS.mode == "training":
+    # raw data preparation
+    batchsize = 4
+    img_path = "/home/students/nanjiang/Data/unit_test/image/*.tif"
+    imagereader = ImageReader(img_path, batchsize)
+    image = imagereader.read_batchimages()
+    label_path = "/home/students/nanjiang/Data/unit_test/label/*.tif"
+    labelreader = LabelReader(label_path, batchsize)
+    label = labelreader.read_batchlabels()
+
+    # structure of NN generation
+    unet = Unet(image.shape, 7)
+    trainer = Trainer(unet.img_seg, n_class=7, learning_rate=1e-6)
+
+    # starting to train
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    sess.run(trainer.train, feed_dict={unet.image:image, trainer.label:label})
 
 
 
